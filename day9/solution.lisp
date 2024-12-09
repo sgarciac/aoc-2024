00f79bccf1758731ac0386d038504b9c84dd6067
sergio <sergio@demarque.com>
2024-12-09 17:49:39 -0500

    day9 part2 (eeek)

--- day9/solution.lisp
+++ day9/solution.lisp
@@ -1,3 +1,4 @@
+;; part1
 (defun read-disk (file)
   (with-open-file (s file)
     (let* ((sizes (loop with line = (read-line s)
@@ -18,14 +19,11 @@
 
 (defun seek-next-empty (disk start)
   "index of closest empty block, starting from start. nil if not found"
-  (loop for i from start below (length disk) while (> (aref disk i) -1) finally (return  (when (< i (length disk) ) i))))
+  (loop for i from start below (length disk) while (> (aref disk i) -1) finally (return (when (< i (length disk)) i))))
 
 (defun seek-prev-file (disk start)
   "index of closest file block, from right to left, starting from start. nil if not found"
   (loop for i from start downto 0 while (= (aref disk i) -1) finally (return (when (>= i 0) i))))
-
-(seek-prev-file (read-disk "test1") 14)
-(aref (read-disk "test1") 14)
 
 (defun defrag (disk)
   "defrag in place"
@@ -42,8 +40,49 @@
 (defun checksum (disk)
   (loop for id across disk
         for i from 0 below (length disk)
-        while (> id -1)
-        summing (* i id)))
+        when (> id -1) summing (* i id)))
 
-;; part1
-(checksum  (defrag (read-input "input")))
+(checksum  (defrag (read-disk "input")))
+
+;; part2
+(defun find-id (id disk)
+  "find first index of file"
+  (loop for start from 0 below (length disk) while (not (= (aref disk start) id)) finally (return start)))
+
+(defun count-id (disk start id)
+  (loop for index from start below (length disk) while (= (aref disk index) id) finally (return (- index start))))
+
+(defun seek-next-empty-opt (disk start end)
+  "index of closest empty block, starting from start and no further than end. nil if not found"
+  (loop for i from start upto end while (> (aref disk i) -1)
+        finally (return (when (< i end) i))))
+
+(defun count-empty (disk start) (count-id disk start -1))
+
+(defun find-empty (disk minsize end)
+  (loop
+    with index = 0
+    for empty = (seek-next-empty-opt disk index end)
+    while empty
+    do (let ((ce (count-empty disk empty)))
+         (if (< ce minsize)
+             (incf index ce)
+             (return-from find-empty empty)))))
+
+
+(defun extract-ids (disk)
+  (remove-duplicates (loop for val across disk when (> val -1) collect val)))
+
+(let* ((disk (read-disk "input"))
+       (ids (extract-ids disk)))
+  (loop for id in (reverse  ids)
+        do (let* ((id-start (find-id id disk))
+                  (size-id (count-id disk id-start id))
+                  (empty-start (find-empty disk size-id id-start)))
+             (when (zerop (mod id 200)) (format t "id:~A id-start:~A size-id:~A~%" id id-start size-id))
+             (when (and empty-start (< empty-start id-start))
+               (loop for i from 0 below size-id
+                     do (setf (aref disk (+ empty-start i)) id
+                              (aref disk (+ id-start i)) -1)))
+             ))
+  (print (checksum disk)))
