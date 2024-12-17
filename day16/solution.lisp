7156a6579ff9474280aaaac719857a89c6dadbb4
sergio <sergio@demarque.com>
2024-12-16 21:31:38 -0500

    day16

--- day16/solution.lisp
+++ day16/solution.lisp
@@ -43,57 +43,82 @@
 (defun turnaround (dir)
   (cons  (* -1 (car dir)) (* -1 (cdr dir))))
 
-(defun init-state (m start)
+(defun init-state (start dir)
   "visited: position -> path / cost / direction"
   (let ((visited (make-hash-table :test #'equal))
         (unvisited (make-hash-table :test #'equal)))
-    (loop for row from 0 below (rows m) do
-      (loop for col from 0 below (cols m)
-            do (let ((pos (cons row col)))
-                 (when (eq (val pos m) :empty)
-                   (setf (gethash pos unvisited)
-                         (if (equal start pos)
-                             (make-cpath :path (list pos) :cost 0 :direction (cons 0 1))
-                             (make-cpath :path nil :cost nil :direction nil)))))))
+    (setf (gethash (cons start dir) unvisited)
+          (make-cpath :path (list start) :cost 0 :direction dir))
     (make-state :visited visited :unvisited unvisited)))
 
 (defun closest-unvisited (unvisited)
   (loop
     with closest = nil
     for candidate being the hash-values in unvisited
-    do (when (and (cpath-cost candidate)
-                  (or (not closest)
-                      (< (cpath-cost candidate) (cpath-cost closest))))
+    do (when (or (not closest)
+                 (< (cpath-cost candidate) (cpath-cost closest)))
          (setf closest candidate))
     finally (return closest)))
 
+(defun dijkstra (map start dir)
+  "returns visited"
+  (let* ((state (init-state start dir)))
+    (loop
+      ;; current = current cpath
+      for current = (closest-unvisited (state-unvisited state))
+      while current
+      do (let* ((pos (car (cpath-path current)))
+                (current-dir (cpath-direction current))
+                (node (cons pos current-dir)))
+           (loop for dir in (list
+                             current-dir
+                             (clockwise current-dir)
+                             (anticlockwise current-dir)
+                             (turnaround current-dir))
+                 for cost in (list 1 1001 1001 1001)
+                 do (let* ((next-pos (next-step pos dir))
+                           (next-node (cons next-pos dir))
+                           (next-cpath (gethash next-node (state-unvisited state))))
+                      (when (eq :empty (val next-pos map))
+                        (when
+                            (and (not (gethash next-node (state-visited state)))
+                                 (or (not next-cpath)
+                                     (< (+ (cpath-cost current) cost) (cpath-cost next-cpath))))
+                          (setf (gethash next-node (state-unvisited state))
+                                (make-cpath
+                                 :path (cons next-pos (cpath-path current))
+                                 :cost (+ (cpath-cost current) cost)
+                                 :direction dir))))))
+           (setf (gethash node (state-visited state)) current)
+           (remhash node (state-unvisited state))))
+    (state-visited state)))
+
+;; part 1
+(defun cpaths (pos visited)
+  (loop for dir in (list (cons -1 0) (cons 0 1) (cons 1 0) (cons 0 -1))
+        for cpath = (gethash (cons pos dir) visited)
+        when cpath collecting cpath))
 
 (let* ((input (read-input "input"))
-       (state (init-state (input-map input) (input-start input))))
-  (loop
-    for current = (closest-unvisited (state-unvisited state))
-    while current
-    do (let ((pos (car (cpath-path current)))
-             (current-dir (cpath-direction current)))
-         (format t "~A ~A~%" (cpath-cost current) pos)
-         (loop for dir in (list
-                           current-dir
-                           (clockwise current-dir)
-                           (anticlockwise current-dir)
-                           (turnaround current-dir))
-               for cost in (list 1 1001 1001 1001)
-               do (let* ((next-pos (next-step pos dir))
-                         (next-cpath (gethash next-pos (state-unvisited state))))
-                    (when next-cpath
-                      (when (or (not (cpath-cost next-cpath))
-                                (< (+ (cpath-cost current) cost) (cpath-cost next-cpath)))
-                        (setf (gethash next-pos (state-unvisited state))
-                              (make-cpath
-                               :path (cons next-pos (cpath-path current))
-                               :cost (+ (cpath-cost current) cost)
-                               :direction dir))))))
-         (setf (gethash pos (state-visited state)) current)
-         (remhash pos (state-unvisited state))))
-  (gethash (input-end input) (state-visited state)))
+       (visited (dijkstra (input-map input) (input-start input) (cons 0 1))))
+  (loop for cpath in (cpaths (input-end input) visited)
+        minimizing (cpath-cost cpath)))
 
-(input-end (read-input "input"))
+;; part 2
+(print  (let* ((input (read-input "input"))
+               (visited (dijkstra (input-map input) (input-start input) (cons 0 1)))
+               (best-final-cost (loop for cpath in (cpaths (input-end input) visited)
+                                      minimizing (cpath-cost cpath))))
+          (loop for node being the hash-keys of visited using (hash-value cpath)
+                for i = 0 then (1+ i)
+                with valid = ()
+                when (< (cpath-cost cpath) best-final-cost)
+                  do (progn (when (zerop (mod i 100))  (print i))
+                            (let* ((tvisited (dijkstra (input-map input) (car node) (cdr node)))
+                                   (best-intermediate-to-end (loop for cpath in (cpaths (input-end input) tvisited)
+                                                                   minimizing (cpath-cost cpath)))
+                                   (best-start-to-intermediate
+                                     (cpath-cost cpath)))
+                              (when (= best-final-cost (+ best-intermediate-to-end best-start-to-intermediate))
+                                (push (car node) valid))))
+                finally (return (1+ (length (remove-duplicates valid :test #'equal)))))))
